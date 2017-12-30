4c34be14d0ee8b6e2da3727cc7e99a92f0846711
Kate McFaul <katemcfaul@gmail.com>
2017-12-30 11:58:38 -0800

    Install and set up Sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles/styles.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
