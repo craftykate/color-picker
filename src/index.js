f7562fabfa50dd0f5c8567ac05f445b2fb3ac4f1
Kate McFaul <katemcfaul@gmail.com>
2017-12-30 12:10:14 -0800

    file housekeeping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import './styles/styles.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
