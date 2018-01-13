3156b610828bfa6b4591c975e528310b01e060b8
Kate McFaul <katemcfaul@gmail.com>
2018-01-12 21:29:35 -0800

    make saved colors totally square

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
 import App from './Components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
